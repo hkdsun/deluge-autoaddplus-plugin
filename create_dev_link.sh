f02a6534d5d1c88915f314e1529072d23530f7cc
Hormoz K <hkheradm@gmail.com>
2020-04-20 17:16:11 -0400

    start migration to deluge2

--- create_dev_link.sh
+++ create_dev_link.sh
@@ -2,7 +2,7 @@
 mkdir -p conf/plugins
 mkdir temp
 export PYTHONPATH=./temp
-python setup.py build develop --install-dir ./temp
+python3 setup.py build develop --install-dir ./temp
 cp ./temp/AutoAddPlus.egg-link ./conf/plugins/
-cp ./vendor/LabelPlus-0.3.2.2-py2.7.egg ./conf/plugins/
+cp ./vendor/LabelPlus-0.3.2.4-py3.6.egg ./conf/plugins/
 rm -fr ./temp
